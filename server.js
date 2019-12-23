67833935885087b728b8d064e75e4a8b622a4480
Alex Griep <alexgriep@hotmail.com>
2019-12-23 16:08:16 -0600

    added more folders and started on models for pokemon individuals and types

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+// Set view engine as handlebars file type
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burger_controller");
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    })
+});
