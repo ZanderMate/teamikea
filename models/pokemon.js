64ba7dd5f850056f5d6095868f48805ba37f925f
Elizkoleary <eliz.k.oleary@gmail.com>
2020-01-04 12:41:15 -0600

    server is now working

--- models/pokemon.js
+++ models/pokemon.js
@@ -1,23 +1,20 @@
-//Dependencies
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references our connection to the DB.
-var sequelize = require("../config/connection.js");
-
+module.exports = function (sequelize, DataTypes) {
 // Creates a "Burger" model that matches up with DB
 var Pokemon = sequelize.define("pokemon", {
-  pokemon_name: Sequelize.STRING,
-  physical_attack: Sequelize.INTEGER,
-  special_attack: Sequelize.INTEGER,
-  physical_defense: Sequelize.INTEGER,
-  special_defense: Sequelize.INTEGER,
-  speed: Sequelize.INTEGER,
-  hitpoints: Sequelize.INTEGER,
-  type1_index: Sequelize.INTEGER,
-  type2_index: Sequelize.INTEGER,
+  pokemon_name: DataTypes.STRING,
+  physical_attack: DataTypes.INTEGER,
+  special_attack: DataTypes.INTEGER,
+  physical_defense: DataTypes.INTEGER,
+  special_defense: DataTypes.INTEGER,
+  speed: DataTypes.INTEGER,
+  hitpoints: DataTypes.INTEGER,
+  type1_index: DataTypes.INTEGER,
+  type2_index: DataTypes.INTEGER,
 });
 
 // Syncs with DB
 Pokemon.sync();
 
 // Makes the Chirp Model available for other files (will also create a table)
-module.exports = Pokemon;
+return Pokemon;
+}
